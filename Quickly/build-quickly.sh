b1fb6238785bfe9b8dae257683d4c162228ac1dc
Agaric Perdereau <contact@neomobili.com>
2020-03-01 16:51:27 +0100

    Added Quickly Theme

---
+++ Quickly/build-quickly.sh
@@ -0,0 +1,10 @@
+lessc quickly-theme.less dist/quickly-theme.css
+lessc salmon-palet.less dist/salmon-palet.css
+lessc sky-palet.less dist/sky-palet.css
+lessc aqua-palet.less dist/aqua-palet.css
+
+cp dist/quickly-theme.css ~/Library/Containers/com.neomobili.slideas.macos/Data/Documents/.Slideas/UserThemes/Quickly
+cp dist/salmon-palet.css ~/Library/Containers/com.neomobili.slideas.macos/Data/Documents/.Slideas/UserThemes/Quickly
+cp dist/sky-palet.css ~/Library/Containers/com.neomobili.slideas.macos/Data/Documents/.Slideas/UserThemes/Quickly
+cp dist/aqua-palet.css ~/Library/Containers/com.neomobili.slideas.macos/Data/Documents/.Slideas/UserThemes/Quickly
+cp theme.json ~/Library/Containers/com.neomobili.slideas.macos/Data/Documents/.Slideas/UserThemes/Quickly
